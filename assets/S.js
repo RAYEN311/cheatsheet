faa3e1819b83099fdc31fd5984765bc88142e2bc
Rayen ben brahim <129397918+RAYEN311@users.noreply.github.com>
2023-07-19 13:22:30 +0100

    Update S.js

--- assets/S.js
+++ assets/S.js
@@ -2,6 +2,6 @@ let track_lang = Array.from(document.getElementsByClassName('language'))
 track_lang.map(lang => {
     lang.addEventListener('click', (e)=>{
        console.log(e.target.id)
-       window.location = "cheatsheet/" + e.target.id
+       window.location = "../cheatsheet/" + e.target.id
     })
 })
